455de01889172eff3d17c3272992b33a2b40f83e
J-Boogie <jackr353@gmail.com>
2020-12-16 11:18:06 -0600

    add js pseudocode

---
+++ script.js
@@ -0,0 +1,18 @@
+// search for a city
+    // build search query --> after button press
+        // format what's entered to make it usable in search query
+        // add apiKey and search url and city name
+        // do search
+    // display results
+        // current conditions in main container
+            // icon to rep conditions
+            // temp, humidity, windspeed
+            // box for UV index
+        // future conditions in bottom 5 boxes
+            // date
+            // icon for weather
+            // temp
+            // humidity
+    // add to search history
+        // add button for each city, make it clickable
+// upon refresh, presetn last searched city forecast
