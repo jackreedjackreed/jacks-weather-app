e7d4d330ee90d3895306dbbb885fe569bf0606d6
J-Boogie <jackr353@gmail.com>
2020-12-16 17:05:23 -0600

    add https DUMBO

--- script.js
+++ script.js
@@ -2,33 +2,34 @@
 cityName = "Nashville"
 
 const apiKey = "d97eef3a7948c38c7df6cf91a2d5c2a8"
-var queryConstants = "api.openweathermap.org/data/2.5/weather?q="
+var queryConstants = "https://api.openweathermap.org/data/2.5/weather?q="
 
 var testing = "api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=d97eef3a7948c38c7df6cf91a2d5c2a8";
-
+emptyArr = [];
 
 // search for a city
-    // build search query --> after button press
-function generalCall(nerm) {
+    // build search query --> after button press (NERM = NAME)
+function getCityObj(nerm) {
     console.log("banana");
      // format what's entered to make it usable in search query
     var queryURL = (queryConstants + nerm + "&appid=" + apiKey);
     console.log(queryURL);
     // do search
-    return queryURL
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+          console.log(response);
+          console.log("apple");
+          return response
+      });
     
 };
 
-var URLL = generalCall(cityName);
+var object = getCityObj(cityName);
+console.log(object);
 
-
-$.ajax({
-    url: URLL,
-    method: "GET"
-  }).then(function(response) {
-      console.log(response);
-      console.log("apple");
-  });
+       
     
         
     // display results
