800e7b35105a0cdd0b92c2373db791e533dcd56d
J-Boogie <jackr353@gmail.com>
2020-12-16 16:18:19 -0600

    making more changes, keep getting 404 errors

--- script.js
+++ script.js
@@ -1,32 +1,37 @@
 
-cityName = "milan"
+cityName = "Nashville"
 
 const apiKey = "d97eef3a7948c38c7df6cf91a2d5c2a8"
-var queryConstants = "api.openweathermap.org/data/2.5/weather?"
+var queryConstants = "api.openweathermap.org/data/2.5/weather?q="
 
-
+var testing = "api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=d97eef3a7948c38c7df6cf91a2d5c2a8";
 
 
 // search for a city
     // build search query --> after button press
-function generalCall(cityName) {
+function generalCall(nerm) {
     console.log("banana");
-    var queryURL = (queryConstants + "q=" + cityName + "&appid=" + apiKey);
+     // format what's entered to make it usable in search query
+    var queryURL = (queryConstants + nerm + "&appid=" + apiKey);
+    console.log(queryURL);
+    // do search
     $.ajax({
         url: queryURL,
         method: "GET"
       }).then(function(response) {
           console.log(response);
+          console.log("apple");
       });
 };
 
 generalCall(cityName);
 
-        // format what's entered to make it usable in search query
-        // add apiKey and search url and city name
-        // do search
+
+       
+    
+        
     // display results
-        // current conditions in main container
+        // current conditions in main container›
             // icon to rep conditions
             // temp, humidity, windspeed
             // box for UV index
