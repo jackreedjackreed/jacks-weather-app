2f5d72890c9f217b709ce286eee90c46afbd03fe
J-Boogie <jackr353@gmail.com>
2020-12-16 17:36:17 -0600

    trying to return response and use it in later fxns

--- script.js
+++ script.js
@@ -5,29 +5,48 @@ const apiKey = "d97eef3a7948c38c7df6cf91a2d5c2a8"
 var queryConstants = "https://api.openweathermap.org/data/2.5/weather?q="
 
 var testing = "api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=d97eef3a7948c38c7df6cf91a2d5c2a8";
-emptyArr = [];
+
 
 // search for a city
     // build search query --> after button press (NERM = NAME)
 function getCityObj(nerm) {
-    console.log("banana");
      // format what's entered to make it usable in search query
     var queryURL = (queryConstants + nerm + "&appid=" + apiKey);
-    console.log(queryURL);
+    // console.log(queryURL);
     // do search
     $.ajax({
         url: queryURL,
         method: "GET"
       }).then(function(response) {
-          console.log(response);
-          console.log("apple");
+          strungObj = JSON.stringify(response);
+          console.log(strungObj[0]);
+          console.log(strungObj);
+          // getDataFromObj(response);
           return response
       });
     
 };
 
-var object = getCityObj(cityName);
-console.log(object);
+
+console.log(getCityObj("nashville"));
+
+function getNameFromObj(strungObj) {
+    var objName = strungObj;
+    console.log(objName + "hey");
+    return objName;
+}
+
+getNameFromObj(getCityObj("nashville"));
+
+function getDataFromObj(data) {
+    let ObjDict = {}
+    var cityName = data.name;
+    console.log(cityName);
+    var cityTemp;
+
+}
+
+
 
        
     
