738a17adc8aded227a934b54539c7a58d46adc2e
J-Boogie <jackr353@gmail.com>
2021-03-17 09:58:23 -0500

    add document.ready function

--- script.js
+++ script.js
@@ -6,64 +6,73 @@ cityName = "Nashville"
 
 const apiKey = "d97eef3a7948c38c7df6cf91a2d5c2a8"
 var queryConstants = "https://api.openweathermap.org/data/2.5/weather?q="
+// var testing = "api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=d97eef3a7948c38c7df6cf91a2d5c2a8";
 
-var testing = "api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=d97eef3a7948c38c7df6cf91a2d5c2a8";
+$(document).ready(function() {
+
+
+
+
 
 
 // search for a city
     // build search query --> after button press (NERM = NAME)
-function getCityObj(name, handleData) {
-     // format what's entered to make it usable in search query
-    var queryURL = (queryConstants + name + "&appid=" + apiKey);
-    // do search
-    ($.ajax({
-        url: queryURL,
-        method: "GET"
-      }).then(handleData)
-        // console.log(response);
-        //   strungObj = JSON.stringify(response);
-        //   console.log(strungObj + " strungObj");
-        //   parseObj = strungObj.JSON.parse();
-        //   console.log(parseObj + " parseObj");
-          // getDataFromObj(response);
-          //return response
-    );
-};
-var CityObj = {}
-getCityObj( "nashville", getDataFromObj);
-console.log(CityObj);                                   /// here, cityobj gets its shit defined bc its global
-var testing = getCityObj("milan", getDataFromObj)
-console.log(testing);
+    function getCityObj(name, handleData) {
+        // format what's entered to make it usable in search query
+       var queryURL = (queryConstants + name + "&appid=" + apiKey);
+       // do search
+       ($.ajax({
+           url: queryURL,
+           method: "GET"
+         }).then(handleData)
+           // console.log(response);
+           //   strungObj = JSON.stringify(response);
+           //   console.log(strungObj + " strungObj");
+           //   parseObj = strungObj.JSON.parse();
+           //   console.log(parseObj + " parseObj");
+             // getDataFromObj(response);
+             //return response
+       );
+   };
+   var CityObj = {}
+   getCityObj( "nashville", getDataFromObj);
+   console.log(CityObj);                                   /// here, cityobj gets its shit defined bc its global
+   var testing = getCityObj("milan", getDataFromObj)
+   console.log(testing);
+   
+   function getDataFromObj( response ) {
+       // name
+       CityObj.name = response.name;
+       // weather
+       CityObj.weather = response.weather[0].main; 
+       // temperature
+       var kTemp = response.main.temp;
+       var fTemp = Math.round(kTemp*1.8 - 459.67)
+       CityObj.temperature = fTemp;
+       // humidity
+       CityObj.humidity = response.main.humidity;
+       // wind speed
+       CityObj.windspeed = response.wind.speed;
+       console.log(CityObj);
+       return CityObj
+   
+   }
+   
+           
+       // display results
+           // current conditions in main container›
+               // icon to rep conditions
+               // temp, humidity, windspeed
+               // box for UV index
+           // future conditions in bottom 5 boxes
+               // date
+               // icon for weather
+               // temp
+               // humidity
+       // add to search history
+           // add button for each city, make it clickable
+   // upon refresh, presetn last searched city forecast
 
-function getDataFromObj( response ) {
-    // name
-    CityObj.name = response.name;
-    // weather
-    CityObj.weather = response.weather[0].main; 
-    // temperature
-    var kTemp = response.main.temp;
-    var fTemp = Math.round(kTemp*1.8 - 459.67)
-    CityObj.temperature = fTemp;
-    // humidity
-    CityObj.humidity = response.main.humidity;
-    // wind speed
-    CityObj.windspeed = response.wind.speed;
-    console.log(CityObj);
-    return CityObj
 
-}
+})
 
-        
-    // display results
-        // current conditions in main container›
-            // icon to rep conditions
-            // temp, humidity, windspeed
-            // box for UV index
-        // future conditions in bottom 5 boxes
-            // date
-            // icon for weather
-            // temp
-            // humidity
-    // add to search history
-        // add button for each city, make it clickable
-// upon refresh, presetn last searched city forecast
